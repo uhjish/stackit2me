973feffb51b8098a2ce5dd520c34aaf244160a0f
Ajish <ajish@rootedinsights.com>
2016-04-25 07:13:17 -0400

    add tweetzonium creds

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -4,10 +4,10 @@ require 'twitter'
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.access_token = 'YOUR_OAUTH_TOKEN'
-  config.access_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_key = 'loj2kbTrHJW0MLyevIMF5tfKf'
+  config.consumer_secret = 'KjjTaDCBfKhx9d3eZZUnrS6AQ9VfKH2glabf1Xt0EaeOU3M9h4'
+  config.access_token = '2908219731-RhilzscuouXWRBFKWUlDmwc3qx3eJJKPcwwk1RO'
+  config.access_token_secret = 'CU75quJrFOxMdhwr9LHBRLIMr74A90TR0lMingSaGPY8V'
 end
 
 search_term = URI::encode('#todayilearned')
